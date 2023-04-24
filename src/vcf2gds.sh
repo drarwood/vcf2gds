e5f2f9ebcfae9060eb4d913715735e3cbf8035c5
arw201 <arw201@slade.isad.isadroot.ex.ac.uk>
2023-04-24 13:19:29 +0100

    Parallel processes now output and passed to R script

--- src/vcf2gds.sh
+++ src/vcf2gds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# vcf2gds 0.0.1
+# vcf2gds 0.0.2
 # Andrew R Wood
 # University of Exeter
 # No warrenty provided!
@@ -7,8 +7,9 @@
 
 main() {
 
-    echo "Input VCF: vcf_file: '$vcf_file'"
-    echo "Output GDS: gds_filename: '$gds_filename'"
+    echo "Input VCF: '$vcf_file'"
+    echo "Output GDS: '$gds_filename'"
+    echo "Number of parallel processes for file conversion: '$parallel'"
 
     # Download the VCF to convert to GDS
     dx download "$vcf_file" -o vcf_file
@@ -17,7 +18,7 @@ main() {
     tar -zxf r_library.tar.gz
 
     # Run the R script to 1) convert VCF to GDS, 2) inject PASS as a QC filter
-    Rscript vcf2gds.R vcf_file gds
+    Rscript vcf2gds.R vcf_file gds $parallel
 
     # Upload the GDS file to the project directory
     gds=$(dx upload gds --brief --path ./$gds_filename)
